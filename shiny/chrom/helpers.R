476b36891ce364f37d759e6efab310e8df587b45
Karol Pal <karolpal.jr@gmail.com>
2015-10-28 17:32:45 +0100

    var calling rule updated

--- shiny/chrom/helpers.R
+++ shiny/chrom/helpers.R
@@ -83,13 +83,15 @@ call_variants <- function(calls, qual_thres, mut_min, s2n_min){
         calls[set_by_user == FALSE, mut_call_rev := call_rev]
         calls[
             mut_peak_pct_fwd >= mut_min
-            & mut_s2n_abs_fwd >= s2n_min
+            & mut_s2n_abs_fwd >= s2n_min 
+            & mut_peak_base_fwd != reference
             #& quality_fwd >= qual_thres
-            ,mut_call_fwd := mut_peak_base_fwd
+            ,mut_call_fwd := mut_peak_base_fwd     #do not change this in case
             ]
         calls[
             mut_peak_pct_rev >= mut_min
             & mut_s2n_abs_rev >= s2n_min
+            & mut_peak_base_rev != reference
             #& quality_rev >= qual_thres
             ,mut_call_rev := mut_peak_base_rev
             ]
