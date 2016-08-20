e974e780de4924e56284ab0e52c0a2e69accd8c8
Karol Pal <karolpal.jr@gmail.com>
2016-08-20 09:47:40 +0200

    dbSNP on TP53, update log

--- ui.R
+++ ui.R
@@ -67,7 +67,7 @@ shinyUI(
 			#column(2,actionButton("mng_samples_btn","Manage/Load samples",style="width:100%;height:20px;padding:0;margin-top:8px;")),
 			column(2, HTML(paste("&nbsp<i>assisted and standardised assessment</br>&nbsp&nbsp&nbspof gene aberrations from Sanger data</i></br></br>"))),
 			column(9, htmlOutput("files")),
-			column(1, HTML(paste(' <a href="javascript:void(0)" id="app-disclaimer-link" onclick="$(\'#disclaimer-modal\').modal(\'show\')">version 0.2.3 <br>(2016-Aug-11)</a>
+			column(1, HTML(paste(' <a href="javascript:void(0)" id="app-disclaimer-link" onclick="$(\'#disclaimer-modal\').modal(\'show\')">version 0.2.4 <br>(2016-Aug-20)</a>
                                 <!-- Update log -->
                                 <div class="modal fade" id="disclaimer-modal" tabindex="-1" role="dialog">
 			                    <div id="disclaimer-modal-content" class="modal-dialog" role="document">
@@ -76,6 +76,10 @@ shinyUI(
 			                    <h4>update log</h4>
 			                    </div>
 			                    <div class="modal-body" style="padding-top:0px;padding-bottom:0px;font-size:12px;color:rgb(132,132,132)">
+                                <b>ver 0.2.4 (2016-Aug-20)</b>
+                                <ul>
+			                    <li>Position based dbSNP annotation added for TP53. (Column "dbSNP" in the variants table.)</li>
+			                    </ul>
                                 <b>ver 0.2.3 (2016-Aug-11)</b>
                                 <ul>
                                 <li>"No reference" upload fixed and documented.</li>
