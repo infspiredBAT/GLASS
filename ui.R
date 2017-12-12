3cc97a9664d1303b64b20f587bb523ac8621aea3
Karol Pal <karolpal.jr@gmail.com>
2017-12-12 16:43:19 +0100

    update log

--- ui.R
+++ ui.R
@@ -33,7 +33,7 @@ shinyUI(
 		fluidRow(
 			column(1, HTML("&nbsp&nbsp<b><font size=6em>GLASS</font></b>")),
 			column(1,HTML('<div  style="padding:.3em .5em"><a href="javascript:void(0)" id="app-disclaimer-link" onclick="$(\'#disclaimer-modal\').modal(\'show\')" >
-                                ver 0.3.1 <br> (2017-Oct-27)
+                                ver 0.3.2 <br> (2017-Dec-12)
                            </a></div>'),
 			       includeHTML("www/log.html")
 			       ),
